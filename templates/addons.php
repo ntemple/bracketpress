1122323b3cab15291a05bb01865a5d50e9f64734
Nick Temple <nick@intellispire.com>
2013-02-14 23:25:52 -0500

    css for addons page

--- templates/addons.php
+++ templates/addons.php
@@ -1,8 +1,28 @@
+<style type="text/css">
+    .addons {
+        width: 600px;
+    }
+    .addons li{
+        float: left;
+        width: 135px;
+        height: 135px;
+        -moz-box-shadow: 10px 10px 5px #888;
+        -webkit-box-shadow: 10px 10px 5px #888;
+        box-shadow: 10px 10px 5px #888;
+        padding: 20px 20px 20px 20px;
+    }
+    .addons li img{
+        xfloat: right;
+    }
+</style>
 <h1>Get BracketPress Add-Ons</h1>
 <br/>
 <a href="http://www.bracketpress.com/extensions">Browse All Add-Ons</a><br/>
-<ul>
+
+<ul class="addons">
+<!--
         <li><a href="http://www.bracketpress.com/downloads/bracketpress-pro-data-feed/" target="store"><img src="http://s3.amazonaws.com/network.intellispire.com/bp-edd/assets/bracketpress-pro-data-feed.png"></a></li>
+-->
         <li><a href="http://www.bracketpress.com/downloads/aweber-newsletter-plugin/" target="store"><img src="http://s3.amazonaws.com/network.intellispire.com/bp-edd/assets/aweber-newsletter-plugin.png"></a></li>
         <li><a href="http://www.bracketpress.com/downloads/icontact-newsletter-plugin/" target="store"><img src="http://s3.amazonaws.com/network.intellispire.com/bp-edd/assets/icontact-newsletter-plugin.png"></a></li>
         <li><a href="http://www.bracketpress.com/downloads/constantcontact-newsletter-plugin/" target="store"><img src="http://s3.amazonaws.com/network.intellispire.com/bp-edd/assets/constantcontact-newsletter-plugin.png"></a></li>
@@ -10,5 +30,3 @@
         <li><a href="http://www.bracketpress.com/downloads/campaignmonitor-newsletter-plugin/" target="store"><img src="http://s3.amazonaws.com/network.intellispire.com/bp-edd/assets/campaignmonitor-newsletter-plugin.png"></a></li>
         <li><a href="http://www.bracketpress.com/downloads/getresponse-newsletter-plugin/" target="store"><img src="http://s3.amazonaws.com/network.intellispire.com/bp-edd/assets/getresponse-newsletter-plugin.png"></a></li>
 </ul>
-
-
